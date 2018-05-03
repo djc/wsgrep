c9fc5ed5c7ab81a2d85e8b49747f6bb4e9be5d66
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-05-03 20:49:38 +0200

    Add match highlighting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate regex;
 #[macro_use]
 extern crate structopt;
+extern crate termion;
 
 use regex::Regex;
+use std::fmt::Write as FmtWrite;
 use std::io::{self, BufRead, Write};
 use structopt::StructOpt;
+use termion::color;
 
 fn main() {
     let config = Config::from_args();
@@ -14,14 +17,28 @@ fn main() {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
 
+    let mut hl_start = String::new();
+    write!(hl_start, "{}", color::Fg(color::Red)).unwrap();
+    let mut hl_end = String::new();
+    write!(hl_end, "{}", color::Fg(color::Reset)).unwrap();
+
     for ln in stdin.lock().lines() {
         let ln = ln.expect("error while reading line");
-        if re.is_match(&ln) {
-            handle
-                .write(ln.as_bytes())
-                .expect("error while writing line");
-            handle.write(b"\n").expect("error while writing newline");
-        }
+        let m = match re.find(&ln) {
+            Some(m) => m,
+            None => continue,
+        };
+
+        let start = m.start();
+        let end = m.end();
+        handle.write(ln[..start].as_bytes()).unwrap();
+
+        handle.write(&hl_start.as_bytes()).unwrap();
+        handle.write(ln[start..end].as_bytes()).unwrap();
+        handle.write(&hl_end.as_bytes()).unwrap();
+
+        handle.write(ln[end..].as_bytes()).unwrap();
+        handle.write(b"\n").unwrap();
     }
 }
 
